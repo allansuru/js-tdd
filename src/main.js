1d2665a52435e6677afb4596ecf057715220449c
AP <allansuru@hotmail.com>
2020-09-18 14:53:02 -0300

    praticando spread operator e rest

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-const a = 3;
 
-console.log(a)
+const name = 'Allan';
+
+const convertToArray = Array.from(name);
+console.log(convertToArray);
+
+const data = [
+    {
+        name: 'Allan'
+    },
+    {
+        name: 'Gui'
+    }
+]
+
+const existe = data.some(item => item.name === 'Gui')
+
+console.log(existe);
+
+
+
+const multiply = (mult, ...args) => args.map(value => mult * value);
+
+
+console.log(multiply(2, 1, 2, 3))
