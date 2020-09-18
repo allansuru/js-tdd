05c604c29142ec748d5dc6d27fd30e46b890532b
AP <allansuru@hotmail.com>
2020-09-18 16:42:49 -0300

    multiple promise OK

--- src/main.js
+++ src/main.js
@@ -24,34 +24,29 @@ console.log(existe);
 const multiply = (mult, ...args) => args.map(value => mult * value);
 
 
-console.log(multiply(2, 1, 2, 3))
+console.log(multiply(2, 1, 2, 3));
+
+const currency = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve({ currency: 'dolar', value: 4.50 })
+    }, 1000);
+});
+
+const countries = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(['Ireland', 'England', 'Scotland']);
+    }, 1000);
+});
+
+const resolveds = Promise.all([currency, countries]);
+
+resolveds.then(itens => {
+    console.log(itens)
+})
 
 
-// var defer = new Promise((resolve, reject) => {
-//     setTimeout(() => {
 
-//         if (true) {
-//             resolve(console.log('RESOLVEU'))
-
-//         } else {
-//             reject(console.log('NAO RESOLVEU'))
-
-//         }
-
-
-
-//     }, 2000);
-// })
-
-// defer
-//     .then(d => {
-//         console.log(d);
-//         return `ronaldo`;
-//     })
-//     .then(d => console.log(d))
-//     .catch(err => console.log(err))
-
-let posts = fetch('https://jsonplaceholder.typicode.com/todos')
+fetch('https://jsonplaceholder.typicode.com/todos')
     .then(response => response.json())
     .then(json => json.filter(item => item.userId === 10))
     .then(d => console.log(d))
@@ -59,5 +54,3 @@ let posts = fetch('https://jsonplaceholder.typicode.com/todos')
 
 
 
-
-console.log(posts)
