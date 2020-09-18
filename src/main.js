c55a2ba38f0a2bb1113ebe5ad030f284b4783327
AP <allansuru@hotmail.com>
2020-09-18 15:42:12 -0300

    estudando promises

--- src/main.js
+++ src/main.js
@@ -23,3 +23,22 @@ const multiply = (mult, ...args) => args.map(value => mult * value);
 
 
 console.log(multiply(2, 1, 2, 3))
+
+
+var defer = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (true) {
+            resolve(console.log('RESOLVEU'))
+        } else {
+            reject(console.log('NAO RESOLVEU'))
+        }
+    }, 2000);
+})
+
+defer
+    .then(data => {
+        console.log(data);
+        return `ronaldo`;
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
