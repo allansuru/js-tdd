9eb18a75351d0514f2018192231ca6026532ec85
AP <allansuru@hotmail.com>
2020-09-19 17:35:40 -0300

    estudando generators yield

--- src/generators.js
+++ src/generators.js
@@ -10,9 +10,15 @@ function ajax(url) {
 
 
 function* ajaxGen() {
-    console.log('Buscando post...')
+    console.log('Buscando post 1...')
     const posts = yield ajax('https://jsonplaceholder.typicode.com/todos/1');
     console.log(posts)
+
+    console.log('Buscando post 2...')
+    const posts2 = yield ajax('https://jsonplaceholder.typicode.com/todos/2');
+    console.log(posts2)
+
+
 
     console.log('Buscando dados do git allansuru');
     const ap = yield ajax('https://api.github.com/users/allansuru');
