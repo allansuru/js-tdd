8e5a460199f1a2e7db84a4ece256102b4d973201
AP <allansuru@hotmail.com>
2020-09-20 12:37:11 -0300

    exemplo do new Map

--- src/exempleSet.js
+++ src/exempleSet.js
@@ -10,3 +10,14 @@ for (const iterator of mySet) {
         console.log(`${iterator}, is a cat`)
     }
 }
+
+let map = new Map();
+
+map.set('Allan', 35);
+map.set('Gui', 4)
+
+console.log(map)
+
+for (const m of map) {
+    console.log(m)
+}
