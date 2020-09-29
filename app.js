31e2686cbde308e9385a3af7d2c9bdee0fdf2be3
AP <allansuru@hotmail.com>
2020-09-29 16:15:36 -0300

    adicionado corretamente o watch e estundando import e exports

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import * as R from 'ramda'
 
+import sum from './src/utils'
+
 const { identity } = R
 R.map(identity, [1, 2, 3])
 
@@ -7,3 +9,4 @@ const removeDuplicate = R.union([1, 2, 3], [2, 3, 4]);
 
 console.log(removeDuplicate);
 
+console.log(sum(1, 4))
--- src/randajs.js
+++ src/randajs.js
@@ -3,7 +3,7 @@ import * as R from 'ramda'
 const { identity } = R
 R.map(identity, [1, 2, 3])
 
-const removeDuplicate = R.union([1, 2, 3], [2, 3, 4]);
+const removeDuplicate = R.union([1, 2, 3, 4, 5], [2, 3, 4, 5]);
 
 console.log(removeDuplicate);
 
