e16bd61be0989b3e6b3d60cd8a14d673df701e2b
AP <allansuru@hotmail.com>
2020-09-29 16:32:31 -0300

    exportando variaveis

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import * as R from 'ramda'
 
-import sum from './src/utils'
+import sum, { sub, multiplicacao, div as dividir, PI } from './src/utils'
 
 const { identity } = R
 R.map(identity, [1, 2, 3])
@@ -10,3 +10,11 @@ const removeDuplicate = R.union([1, 2, 3], [2, 3, 4]);
 console.log(removeDuplicate);
 
 console.log(sum(1, 4))
+
+console.log(sub(2, 1))
+
+console.log(multiplicacao(2, 2))
+
+console.log(dividir(2, 2))
+
+console.log(PI)
--- src/utils.js
+++ src/utils.js
@@ -4,5 +4,29 @@ function sum(a, b) {
     return a + b;
 }
 
+// named export
+// ter vários export dentro de uma msm arquivo
+// só pode chamar com o mesmo nome entre chaves{}
+export function sub(a, b) {
+    return a - b;
+}
 
+function mult(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+const PI = 3.14
+
+export { mult as multiplicacao, div, PI }
+
+
+
+// método principal
+// somente um default por arquivo
+// importar com qq nome
+// nao precisa utilizar as chaves
 export default sum;
