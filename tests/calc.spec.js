b4332ec2546756a7ff5e4d1a68c450236a7a61a8
SuRu <allansuru@hotmail.com>
2020-10-10 14:49:34 -0300

    watch test package.json

---
+++ tests/calc.spec.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect
+var calc = require('../src/calc')
+
+describe('Calc', function () {
+
+    // smoke tests
+    describe('Smoke tests', function () {
+
+        it('should exist the calc libs', function () {
+            expect(calc).to.exist
+        });
+
+        it('should exist method `sum`', function () {
+
+        });
+    })
+})
