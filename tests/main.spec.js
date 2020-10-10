78f55887e1fd196371e47ef1af60586532d0d55a
SuRu <allansuru@hotmail.com>
2020-10-10 13:34:43 -0300

    skip, only no mocha

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,12 @@
 describe('Main', function () {
-    describe('metod', function () {
+    describe('metod A', function () {
+        context.skip("case 1", function () {
+            it('should OK', function () {
+                throw new Error('Error metod A, case 1')
+            })
+        })
+    })
+    describe('metod B', function () {
         context("case 1", function () {
             it('should OK', function () {
 
