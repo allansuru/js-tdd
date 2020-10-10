5205d7d3e245d1834616e4d2b48a2cc9118de7ef
SuRu <allansuru@hotmail.com>
2020-10-10 17:39:28 -0300

    basic method calc expects

--- tests/calc.spec.js
+++ tests/calc.spec.js
@@ -27,4 +27,30 @@ describe('Calc', function () {
             expect(calc.div).to.exist;
         });
     })
+
+    describe('Methods', function () {
+        describe('Sum', function () {
+            it('should return 4 when `sum(2,2)`', function () {
+                expect(calc.sum(2, 2)).to.equal(4)
+            });
+        });
+
+        describe('Sub', function () {
+            it('should return 0 when `sub(2,2)`', function () {
+                expect(calc.sub(2, 2)).to.equal(0)
+            });
+        });
+
+        describe('Sub', function () {
+            it('should return 4 when `sub(2,2)`', function () {
+                expect(calc.mult(2, 2)).to.equal(4)
+            });
+        });
+
+        describe('Div', function () {
+            it('should return 2 when `div(4,2)`', function () {
+                expect(calc.div(4, 2)).to.equal(2)
+            });
+        });
+    })
 })
