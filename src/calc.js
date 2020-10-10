492fafe1464e8d8b17e83542218979db8751b4f7
SuRu <allansuru@hotmail.com>
2020-10-10 14:57:43 -0300

    adicionando smoke tests

---
+++ src/calc.js
@@ -0,0 +1,18 @@
+
+
+
+// eslint-disable-next-line no-undef
+module.exports = {
+    sum: function (a, b) {
+        return a + b;
+    },
+    sub: function (a, b) {
+        return a - b;
+    },
+    mult: function (a, b) {
+        return a * b
+    },
+    div: function (a, b) {
+        return a / b;
+    }
+}
--- tests/calc.spec.js
+++ tests/calc.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 var expect = require('chai').expect
 var calc = require('../src/calc')
 
@@ -11,7 +12,19 @@ describe('Calc', function () {
         });
 
         it('should exist method `sum`', function () {
+            expect(calc.sum).to.exist;
+        });
 
+        it('should exist method `sub`', function () {
+            expect(calc.sub).to.exist;
+        });
+
+        it('should exist method `mult`', function () {
+            expect(calc.mult).to.exist;
+        });
+
+        it('should exist method `div`', function () {
+            expect(calc.div).to.exist;
         });
     })
 })
