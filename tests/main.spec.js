45436b8de0ca177a549d7cbd53a106a79675029a
SuRu <allansuru@hotmail.com>
2020-10-10 13:44:52 -0300

    entendendo hooks do mocha

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,8 +1,36 @@
+
 describe('Main', function () {
+
+    // roda uma vez antes do bloco
+    before(function () {
+        console.log('before')
+    })
+
+    // roda uma vez depois do bloco
+    after(function () {
+        console.log('after')
+    })
+
+    // roda todas as vezes, antes de cada bloco
+    beforeEach(function () {
+        console.log('beforeEach')
+    })
+
+    afterEach(function () {
+        console.log('afterEach')
+    })
+
+
     describe('metod A', function () {
         context.skip("case 1", function () {
             it('should OK', function () {
                 throw new Error('Error metod A, case 1')
+            })
+        })
+
+        context("case 2", function () {
+            it('test1', function () {
+
             })
         })
     })
