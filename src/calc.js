28ca7cc7cc4a53c418c39a8e02eed53313360de8
SuRu <allansuru@hotmail.com>
2020-10-11 14:24:20 -0300

    removendo testes para barrar no pre-push do husky

--- src/calc.js
+++ src/calc.js
@@ -8,13 +8,10 @@ const sum = (a, b) => a + b;
 const sub = (a, b) => a - b;
 const mult = (a, b) => a * b;
 const div = (a, b) => (b === 0) ? 'it`s not possible divide by zero' : a / b;
-const outro = () => 'bla'
-const outro2 = () => 'bla'
+
 export {
     sum,
     sub,
     mult,
     div,
-    outro,
-    outro2
 }
